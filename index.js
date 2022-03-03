45171d336bd088be43abcfa2f64e35a837dc5d6d
unknown <mlebedko@sdvor.com>
2022-03-03 12:01:21 +0500

    2 задание из 2 практики

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function createCounter(data,prop){
+function pickPropArray(data,prop){
     const result = [];
     for (let item of data.values()){
         if (item[prop] != null) result.push(item[prop])
@@ -11,10 +11,24 @@ const students = [
     { name: 'Иван', age: 20 },
     { name: 'Эдем', age: 20 },
     { name: 'Денис', age: 20 },
-    { name: 'Виктория', age: 20 },
+    { name: 'Виктория', age: 20, city:'tmn' },
     { age: 40 },
+    
 ]
 
-const result = createCounter(students, 'name')
+const result = pickPropArray(students, 'city')
 console.log(result)
     
+
+function createCounter(){
+    let count = 0
+    return function () {
+        count++
+        console.log(count)
+        return count
+    }
+}
+
+const counter1=createCounter()
+counter1()
+counter1()
