6bceb95c6d4ed01e234e6f958e5859e707c7b02c
Maks <lebedkom03@gmail.com>
2022-03-20 16:53:50 +0500

    3 практика

--- components/list-items.js
+++ components/list-items.js
@@ -2,11 +2,11 @@ export default class ListItems{
     constructor(el,data) {
         this.el=el;
         this.data=data;
-        this.init()
+        this.init();
     }
     init(){
+        this.render();
         const parents=this.el.querySelectorAll('[data-parent]')
-        console.log(parents)
         if ( parents.length !== 0){
             parents.forEach(parent => {
                 const open=parent.querySelector('[data-open]');
@@ -14,20 +14,33 @@ export default class ListItems{
             })
         }
     }
-    
-    render(){
-        this.el.insertAdjacentElement('beforeend',this.renderParent(this.data))
-    }
-    
-    renderParent(data){
-        
-    }
-    
-    renderChildren(data){
-        
-    }
-    
     toggleItems(parent){
         parent.classList.toggle('list-item_open');
+    }
+    render(){
+        this.el.insertAdjacentHTML('beforeend', this.renderParent(this.data));
+    }
+    
+    renderParent(data) {
+        if (data.hasChildren){
+            let currentNode="<div class=\"list-item list-item_open\" data-parent>\n"+
+                "<div class=\"list-item__inner\">\n"+
+                "<img class=\"list-item__arrow\" src=\"./assets/img/chevron-down.png\" alt=\"chevron-down\""+
+                " data-open>\n" +"<img class=\"list-item__folder\"" +
+                " src=\"./assets/img/folder.png\" alt=\"folder\">\n"+"<span>"+data.name+"</span>"
+                +"</div>\n"+"<div class=\"list-item__items\">"
+            
+            data.items.forEach(node=>{
+                currentNode+=this.renderParent(node);
+            });
+            return currentNode+"</div> </div>"
+        }   
+        else
+        {
+            return "<div class=\"list-item__inner\">\n"+
+            "<img class=\"list-item__folder\"" +
+                " src=\"./assets/img/folder.png\" alt=\"folder\">\n"+ 
+                "<span>"+data.name+"</span>\n"+"</div>"
+        }
     }
 } 
--- index.js
+++ index.js
@@ -8,56 +8,73 @@ if (document.readyState==='loading'){
 
 function init(){
     const data={
-        
+        name: 'Каталог товаров',
+        hasChildren: true,
+        items: [
+            {
+                name: 'Мойки',
+                hasChildren: true,
+                items: [
+                    {
+                        name: 'Ulgran1',
+                        hasChildren: true,
+                        items: [
+                            {
+                                name: 'SMT1',
+                                hasChildren: false,
+                                items: []
+                            },
+                            {
+                                name: 'SMT2',
+                                hasChildren: false,
+                                items: []
+                            }
+                        ]
+                    },
+                    {
+                        name: 'Ulgran2',
+                        hasChildren: true,
+                        items: [
+                            {
+                                name: 'SMT3',
+                                hasChildren: false,
+                                items: []
+                            },
+                            {
+                                name: 'SMT4',
+                                hasChildren: false,
+                                items: []
+                            }
+                        ]
+                    }
+                ]
+            },{
+                name: 'Фильтры',
+                hasChildren: true,
+                items: [
+                    {
+                        name: 'Ulgran3',
+                        hasChildren: true,
+                        items: [
+                            {
+                                name: 'SMT5',
+                                hasChildren: false,
+                                items: []
+                            },
+                            {
+                                name: 'SMT6',
+                                hasChildren: false,
+                                items: []
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
     };
-    const items=new ListItems(document.getElementById('list-items'),data);
-    items.render();
+
+    new ListItems(document.getElementById('list-items'),data);  
 }
 
-
-
-// function listHtml(children) {
-//     return  '<ul>' + children.map(node =>
-//             '<li>' + node.name +
-//             (node.type === 'file' ? '' : listHtml(node.children)) +
-//             '</li>').join('\n') +
-//         '</ul>';
-// }
-//
-// // Sample data
-// var data = [
-//     { name: 'Root', type: 'folder', children: [
-//             { name: 'includes', type: 'folder', children: [
-//                     { name: 'header.php', type: 'file' },
-//                     { name: 'footer.php', type: 'file' },
-//                 ]},
-//             { name: 'css', type: 'folder', children: [
-//                     { name: 'images', type: 'folder', children: [
-//                             { name: 'logos', type: 'folder', children: [
-//                                     { name: 'white.svg', type: 'file' },
-//                                     { name: 'dark.svg', type: 'file' },
-//                                 ]},
-//                             { name: 'gallery', type: 'folder', children: [
-//                                     { name: '1.jpg', type: 'file' },
-//                                     { name: '2.jpg', type: 'file' },
-//                                     { name: '3.jpg', type: 'file' },
-//                                     { name: '4.jpg', type: 'file' },
-//                                     { name: '5.jpg', type: 'file' },
-//                                 ]},
-//                             { name: 'contact-page.jpg', type: 'file' },
-//                         ]},
-//                     { name: 'style.css', type: 'file' },
-//                 ]},
-//             { name: 'js', type: 'folder', children: [
-//                     { name: 'jquery.js', type: 'file' },
-//                     { name: 'main.js', type: 'file' },
-//                 ]},
-//             { name: 'index.php', type: 'file' },
-//             { name: 'contact.php', type: 'file' },
-//             { name: 'about-us.php', type: 'file' },
-//         ]}
-// ];
-//
-//
 
 
